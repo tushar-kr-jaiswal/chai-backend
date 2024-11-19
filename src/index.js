a54828a270c1d33380884a3ef46f6aa355ae1c64
Tushar Kumar Jaiswal <tusharjais03@gmail.com>
2024-11-19 16:52:15 +0530

    feat: initial project setup with MongoDB connection

--- src/index.js
+++ src/index.js
@@ -1,35 +1,13 @@
 // require("dotenv").config({ path: "./env" });
-import connectDB from "./db/index.js";
 import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
 dotenv.config({
-  path: "./env",
+path: "./env",
 });
 
+
 connectDB();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -49,16 +27,16 @@ connectDB();
 // const app = express();
 // (async () => {
 //   try {
-//     await mongoose.connect(`${process.env.MONGODB_URI}`);
-//     app.on("error", () => {
-//       console.log("Error");
+//     await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log("ERROR : ", error);
 //       throw error;
 //     });
+
 //     app.listen(process.env.PORT, () => {
 //       console.log(`App is listening on port ${process.env.PORT}`);
 //     });
 //   } catch (error) {
-//     console.log("Error: ", error);
-//     throw err;
+//     console.error("ERROR : ", error);
 //   }
 // })();
