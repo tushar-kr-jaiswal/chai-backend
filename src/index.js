695fec6e3852f0c66943b3a41a8bcd3c5eae7409
Tushar Kumar Jaiswal <tusharjais03@gmail.com>
2024-12-14 16:17:11 +0530

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -3,21 +3,19 @@ import connectDB from "./db/index.js";
 dotenv.config({
   path: "./env",
 });
-connectDB();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port: ${process.env.PORT}`);
+    });
+    app.on("error", (error) => { // listening for error
+      console.log(`Error in src/index.js: ${error}`);
+    });
+  })
+  .catch((err) => {
+    console.log(`MONGODB CONNECTION ERROR src/index.js---: ${err}`);
+  });
 
 // import express from "express";
 // const app = express();
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+
+
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+export { asyncHandler };
+
+
+
+
+
+//  try catch approach
+// const asyncHandler = (fn) => async (req, res, next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({ success: false, message: err.message });
+//     }
+// };
+
+// const asyncHandler =  () => {};
+// const asyncHandler = () => { () => {} };
+// const asyncHandler = () => () => {};
