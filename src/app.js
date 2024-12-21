f42e679fcee2de8fb41a8e7ae5df05b8b507e4d8
Tushar Kumar Jaiswal <tusharjais03@gmail.com>
2024-12-21 19:53:21 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -13,4 +13,14 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+
+
+// Routes
+import userRouter from "./routes/user.routes.js";
+
+
+// Routes Declaration
+app.use("/api/v1/users",userRouter)
+
+//http://localhost:8000/api/v1/users/register
 export { app };
