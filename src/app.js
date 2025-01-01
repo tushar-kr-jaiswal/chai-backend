d62e312601774d4f3ff655fd9a875738678d773a
Tushar Kumar Jaiswal <tusharjais03@gmail.com>
2025-01-01 13:11:37 +0530

    fixed al register controller bugs

--- src/app.js
+++ src/app.js
@@ -23,6 +23,6 @@ import userRouter from "./routes/user.routes.js";
 //  routes declaration
 app.use('/api/v1/users',userRouter);
 
-// http:localhost:8000/api/v1/users/register
+// http://localhost:8000/api/v1/users/register    
 
 export { app };
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -16,7 +16,7 @@ const registerUser = asyncHandler(async (req, res) => {
     // return res
 
     const { fullName, email, username, password } = req.body;
-    console.table(fullName, email, username, password);
+    // console.table(fullName, email, username, password);
 
     if (
         [fullName, username, email, password].some(
@@ -29,7 +29,7 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new ApiError(404, "@ is required in email.");
     }
 
-    const existedUser = User.findOne({
+    const existedUser = await User.findOne({
         // checking if the user already created or not
         $or: [{ username }, { email }],
     });
@@ -39,7 +39,16 @@ const registerUser = asyncHandler(async (req, res) => {
     }
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+
+    if (
+        req.files &&
+        Array.isArray(req.files.coverImage) &&
+        req.files.coverImage.length > 0
+    ) {
+        coverImageLocalPath = req.files.coverImage[0].path;
+    }
 
     if (!avatarLocalPath) {
         throw new ApiError(400, "Avatar file is required!!!");
